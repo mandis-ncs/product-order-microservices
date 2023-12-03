9b14e4a307588c55eb902a50a43b1f478e34b581
mandis-ncs <amnd.castro@gmail.com>
2023-12-03 19:52:58 -0300

    pt 3 finished, copy from techprogrammie bcs of version conflitct

--- microservices/order-service/src/main/java/com/mandis/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mandis.orderservice.repository;
-
-import com.mandis.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
--- microservices/product-service/src/main/java/com/mandis/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mandis.productservice.repository;
-
-import com.mandis.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
